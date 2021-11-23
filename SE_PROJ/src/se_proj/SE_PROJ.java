9beff797b221db58e5d2b93ecade44e6064d8609
aprovenza1 <94625544+aprovenza1@users.noreply.github.com>
2021-11-23 17:30:27 +0100

    Add files via upload

---
+++ SE_PROJ/src/se_proj/SE_PROJ.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se_proj;
+
+/**
+ *
+ * @author Alberto
+ */
+public class SE_PROJ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
