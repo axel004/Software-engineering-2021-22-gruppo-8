f79243cb47cbaeff5f1c7074d7e507fcde8c270a
paola <paola@LAPTOP-PAOLA.mshome.net>
2021-11-25 18:16:51 +0100

    Creazione della classe Complex per la gestione dei numeri complessi

---
+++ Calculator/src/calculator/Complex.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author paola
+ */
+public class Complex {
+    private double real;
+    private double complex;
+
+    public Complex(double real, double complex) {
+        this.real = real;
+        this.complex=complex;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getComplex() {
+        return complex;
+    }
+
+    public void setReal(double real) {
+        this.real = real;
+    }
+
+    public void setComplex(double complex) {
+        this.complex = complex;
+    }
+}
