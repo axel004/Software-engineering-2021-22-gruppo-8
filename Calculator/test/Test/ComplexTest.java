733abbf151a25b35a5b555b6eaab302cd90289c8
paola <paola@LAPTOP-PAOLA.mshome.net>
2021-11-26 10:26:09 +0100

    Creazione della classe ComplexTest per testare la classe complex

---
+++ Calculator/test/Test/ComplexTest.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package Test;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+/**
+ *
+ * @author paola
+ */
+public class ComplexTest {
+   
+
+}
