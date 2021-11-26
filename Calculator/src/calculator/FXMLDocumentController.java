4c2f397fca5656afdc81abf90f7c70ee98728ce9
Eugenio <e.pezzulo@studenti.unisa.it>
2021-11-26 21:03:15 +0100

    User Interface - add top labels

--- Calculator/src/calculator/FXMLDocumentController.java
+++ Calculator/src/calculator/FXMLDocumentController.java
@@ -6,7 +6,11 @@
 package calculator;
 
 import java.net.URL;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -56,9 +60,11 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Label value12;
     
+    private StackCalc stack = new StackCalc();
+    private LinkedList<Label> labels = new LinkedList<>();
     
     private void handleButtonAction(ActionEvent event) {
-
+        
     }
     
     @Override
@@ -66,21 +72,42 @@ public class FXMLDocumentController implements Initializable {
         resetBtn.disableProperty().bind(Bindings.isEmpty(textArea.textProperty()));
         submitBtn.disableProperty().bind(Bindings.isEmpty(textArea.textProperty()));
         currentValue.textProperty().bindBidirectional(textArea.textProperty());
+        
+        labels.add(value1);
+        labels.add(value2);
+        labels.add(value3);
+        labels.add(value4);
+        labels.add(value5);
+        labels.add(value6);
+        labels.add(value7);
+        labels.add(value8);
+        labels.add(value9);
+        labels.add(value10);
+        labels.add(value11);
+        labels.add(value12);
+        
     }    
 
     @FXML
     private void submit(ActionEvent event) {
-        // invalid input -> notify the user and clear textArea
-        Alert alert = new Alert(Alert.AlertType.ERROR, "Invalid input. Please retry", ButtonType.OK);
-        alert.showAndWait();
-        
-        if(alert.getResult() == ButtonType.OK) {
-            textArea.clear();
-        }
         // valid input -> checks if the input is an operand or an operator 
-            // if it's an operand then create a new complex number and push the complex number into stack
-            // if it's an operator call the operator manager
-        System.out.println("click on submitBtn, do something..");
+        // if it's an operand then create a new complex number and push the complex number into stack
+        // if it's an operator call the operator manager
+        Alert alert = new Alert(Alert.AlertType.ERROR, "Invalid input, please retry.", ButtonType.OK);
+        try {
+            checkComplex(textArea.getText());
+            System.out.println("Insert: "+stack.lastElement());
+            System.out.println("size: "+stack.size());
+            updateTopLabel();
+            textArea.clear();
+           
+        } catch (NumberFormatException | NullPointerException e) {
+
+            alert.showAndWait();
+            if (alert.getResult() == ButtonType.OK) {
+                textArea.clear();
+            }
+        }
 
     }
 
@@ -90,4 +117,46 @@ public class FXMLDocumentController implements Initializable {
         textArea.clear();
     }
     
+    private void updateTopLabel() {
+        // if size >=12 --> print first 12 values
+        // else if size < 12 --> print first size values
+        int i=0;
+        for (int j=stack.size()-1; j>=0; j--) {
+            if (i==12)
+                break;
+            labels.get(i).setVisible(true);
+            labels.get(i).setText(stack.get(j).toString());
+            i++;
+        }
+
+    }
+    
+        /*
+    metodo che verifica la correttezza del numero complesso inserito dall'utente
+    ritorna il numero complesso 
+     */
+    public void checkComplex(String numComplex) throws NumberFormatException, NullPointerException {
+        String number[], num = "";
+        double real, complex;
+        numComplex = numComplex.replace("j", "");
+        if (numComplex.contains("+")) {
+            number = numComplex.split(Pattern.quote("+"));
+            real = Double.parseDouble(number[0]);
+            complex = Double.parseDouble(number[1]);
+        }
+        else if (numComplex.contains("-")) {
+            if (numComplex.startsWith("-")){
+                numComplex = numComplex.substring(1);
+                num = "-";
+            }
+            number = numComplex.split("-");
+            real = Double.parseDouble(num.concat(number[0]));
+            complex = Double.parseDouble("-".concat(number[1]));
+        } else {
+            real = Double.parseDouble(numComplex);
+            complex = 0;
+        }
+        stack.add(new Complex(real, complex));
+    }
 }
+
