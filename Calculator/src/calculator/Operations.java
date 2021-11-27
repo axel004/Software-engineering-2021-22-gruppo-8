c66e45f970fcf962a552f1cec1ebfb608e8dfb27
Alberto <Alberto@DESKTOP-ECKL0U5>
2021-11-27 13:03:11 +0100

    aggiunti blocchi try catch per alcune operazioni

--- Calculator/src/calculator/Operations.java
+++ Calculator/src/calculator/Operations.java
@@ -14,14 +14,20 @@ import calculator.Complex;
 public class Operations {
   
     public Complex reverseSign(Complex value) {
-        double real = value.getReal();
-        double complex = value.getComplex();
-        Complex result = new Complex(0,0);
-        if (real != 0)
-            result.setReal(real*-1);
-        if (complex != 0)
-            result.setComplex(complex*-1);       
-        return result;
+        try{
+            double real = value.getReal();
+            double complex = value.getComplex();
+            Complex result = new Complex(0,0);
+            if (real != 0)
+                result.setReal(real*-1);
+            if (complex != 0)
+                result.setComplex(complex*-1);       
+            return result;
+        }
+        catch(Exception e){
+            System.out.print("Reverse sign error\n");
+            return null;
+        }
     }
     
     public Complex multiply(Complex a, Complex b){
@@ -54,10 +60,16 @@ public class Operations {
     }
 
     public Complex differenza(Complex val1, Complex val2){
-        Complex totaldiff = new Complex(val1.getReal()-val2.getReal(),val1.getComplex()-val2.getComplex());  //eseguo la differenza
-        totaldiff.setReal(Math.round(totaldiff.getReal()*10000)/10000.0);
-        totaldiff.setComplex(Math.round(totaldiff.getComplex()*10000)/10000.0);
-        return totaldiff;
+        try{
+            Complex totaldiff = new Complex(val1.getReal()-val2.getReal(),val1.getComplex()-val2.getComplex());  //eseguo la differenza
+            totaldiff.setReal(Math.round(totaldiff.getReal()*10000)/10000.0);
+            totaldiff.setComplex(Math.round(totaldiff.getComplex()*10000)/10000.0);
+            return totaldiff;
+        }
+        catch(Exception e){
+            System.out.print("Diff error\n");
+            return null;
+        }
      
     }   
     
@@ -74,12 +86,12 @@ public class Operations {
         else{
         double abs= Math.sqrt(val.getReal()*val.getReal()+val.getComplex()*val.getComplex());   //calcolo modulo e fase
         double arg = (2*Math.PI + Math.atan2(val.getComplex(),val.getReal())) % (2*Math.PI);
-        
+
         val.setReal(Math.sqrt(abs)*(Math.cos((arg/2))));        //calcolo i valori reali e immaginari e li inserisco nell'oggetto val
         val.setComplex(Math.sqrt(abs)*(Math.sin(arg/2)));
         val.setReal(Math.round(val.getReal()*10000)/10000.0);
         val.setComplex(Math.round(val.getComplex()*10000)/10000.0);
-        
+
         }
       
         return val;
