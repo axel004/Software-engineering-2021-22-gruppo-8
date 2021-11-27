afce9a2f5c8be1d524e0560da96130a7e0047960
Alberto <Alberto@DESKTOP-ECKL0U5>
2021-11-27 12:50:19 +0100

    aggiustamento classe Operator

--- Calculator/src/calculator/Operator.java
+++ Calculator/src/calculator/Operator.java
@@ -26,7 +26,7 @@ public class Operator {
                 if(e.size()>=2){            //controllo se ci sono almeno due valori
                     val2 = e.pop();
                     val1 = e.pop();         //ottengo i due valori dallo stackj
-                    //e.push(o.differenza(val1,val2)); //inserisco il nuovo valore
+                    e.push(o.sum(val1,val2)); //inserisco il nuovo valore
                     return true;
                 }
                 else
@@ -44,7 +44,7 @@ public class Operator {
                 if(e.size()>=2){            //controllo se ci sono almeno due valori
                     val2 = e.pop();
                     val1 = e.pop();         //ottengo i due valori dallo stackj
-                    e.push(o.differenza(val1,val2)); //inserisco il nuovo valore
+                    //e.push(o.div(val1,val2)); //inserisco il nuovo valore
                     return true;
                 }
                 else
@@ -53,16 +53,15 @@ public class Operator {
             if(e.size()>=2){            //controllo se ci sono almeno due valori
                     val2 = e.pop();
                     val1 = e.pop();         //ottengo i due valori dallo stackj
-                    e.push(o.differenza(val1,val2)); //inserisco il nuovo valore
+                    e.push(o.multiply(val1,val2)); //inserisco il nuovo valore
                     return true;
                 }
                 else
                     return false; 
             case "+-":
-                if(e.size()>=2){            //controllo se ci sono almeno due valori
-                    val2 = e.pop();
+                if(e.size()>=1){            //controllo se ci sono almeno due valori
                     val1 = e.pop();         //ottengo i due valori dallo stackj
-                    e.push(o.differenza(val1,val2)); //inserisco il nuovo valore
+                    e.push(o.reverseSign(val1)); //inserisco il nuovo valore
                     return true;
                 }
                 else
